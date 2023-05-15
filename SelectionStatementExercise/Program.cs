b0354589a771fb7825cc429b3ec77b3252a9efdb
Katherine Vail <jabberduckygame@gmail.com>
2023-05-15 14:27:49 -0500

    Completed exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Guess();
         }
+        static void Guess()
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 10);
+           
+
+            Console.WriteLine("Hello guess what my favorite number is!");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > favNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+            else if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low!");
+            }
+            else
+            {
+                Console.WriteLine("That's correct, good job!");
+            }
+        }
+
     }
 }
